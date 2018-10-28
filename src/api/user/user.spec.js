1a67049950413aa894899b77cced913fbcd70a4d
eunwoo <aufcl4858@naver.com>
2018-10-28 14:42:30 +0900

    유저 생성 테스트 케이스 추가

--- src/api/user/user.spec.js
+++ src/api/user/user.spec.js
@@ -1,3 +1,62 @@
+const should = require('should');
+const request = require('supertest');
+const app = require('../../../app');
+
+describe('POST /user는 ', () => {
+  let name = 'testUser',
+    password = 'test123',
+    passwordValid = 'test123',
+    body;
+  describe('성공시 ', () => {
+    before(() => models.sequelize.sync({ force: true }));
+    it('201을 리턴한다', done => {
+      request(app)
+        .post('/user')
+        .send({ name, password, passwordValid })
+        .expect(201)
+        .end((err, res) => {
+          body = res.body;
+          done();
+        });
+    });
+    it('유저 객체를 반환한다', () => {
+      body.should.have.property('id');
+    });
+  });
+  describe('실패시 ', () => {
+    it('name 파라미터가 없을경우 400 반환한다', done => {
+      request(app)
+        .post('/user')
+        .send({ password, passwordValid })
+        .expect(400)
+        .end(done);
+    });
+    it('password 파라미터가 없을경우 400 반환한다', done => {
+      request(app)
+        .post('/user')
+        .send({ name, passwordValid })
+        .expect(400)
+        .end(done);
+    });
+    it('passwordValid 파라미터가 없을경우 400 반환한다', done => {
+      request(app)
+        .post('/user')
+        .send({ name, password })
+        .expect(400)
+        .end(done);
+    });
+    it('password가 다를경우 400 반환한다', done => {
+      (password = 'test123'),
+        (passwordValid = 'test456'),
+        request(app)
+          .post('/user')
+          .send({ name, password, passwordValid })
+          .expect(400)
+          .end(done);
+    });
+  });
+});
+
 /* 유저 생성
 * @method POST
 * @param (name, password, validPassword)
