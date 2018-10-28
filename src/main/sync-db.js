5feab0863d339d91ebddf6236fa90338dcb27d2b
eunwoo <aufcl4858@naver.com>
2018-10-28 23:31:02 +0900

    git 에서 유실된 작업 내용 업로드

--- src/main/sync-db.js
+++
@@ -1,8 +0,0 @@
-const models = require('../../models');
-
-module.exports = () => {
-  const options = {
-    force: process.env.NODE_ENV === 'test'
-  };
-  return models.sequelize.sync(options);
-};
--- src/main/www.js
+++
@@ -1,6 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.listen(3000, () => {
-  console.log('node server start on port 3000');
-});
