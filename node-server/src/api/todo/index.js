34d302bcdd4db480f65a8233a918b5637f71d353
eunwoo <eunwoo@finda.co.kr>
2018-11-01 14:15:07 +0900

    todo 업데이트 로직 구현 및 테스트 성공

--- node-server/src/api/todo/index.js
+++ node-server/src/api/todo/index.js
@@ -4,5 +4,6 @@ const ctrl = require('./todo.ctrl');
 
 // todo 생성
 router.post('/', ctrl.create);
+router.put('/', ctrl.update);
 
 module.exports = router;
--- node-server/src/api/todo/todo.ctrl.js
+++ node-server/src/api/todo/todo.ctrl.js
@@ -13,6 +13,27 @@ const create = (req, res) => {
   });
 };
 
+const update = (req, res) => {
+  if (!req.user) return res.status(400).send({ msg: message.MSG_USERID_MISSING });
+  const userId = req.user.id;
+
+  const title = req.body.title;
+  if (!title) return res.status(400).send({ msg: message.MSG_TITLE_MISSING });
+
+  const id = req.body.id;
+  if (id !== 0 && !id) return res.status(400).send({ msg: message.MSG_ID_MISSING });
+
+  Todo.findOne({ where: { id } }).then(todo => {
+    if (!todo) return res.status(404).end();
+
+    todo.title = title;
+    todo.save().then(todo => {
+      res.status(200).json(todo);
+    });
+  });
+};
+
 module.exports = {
   create,
+  update,
 };
